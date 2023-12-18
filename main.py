4b45143f44b14b6f3bfa3f662d0f41da458dddba
Rommel <karlsrommel255@gmail.com>
2023-12-18 04:34:30 -0600

    Python0010

---
+++ main.py
@@ -0,0 +1,37 @@
+nameUser = input("What is your name? ")
+print("Hello " + nameUser + "!")
+todaysDate = input("What is today's date? ")
+print("Today is " + todaysDate + ".")
+Things = input("tell me your favorite activities")
+if Things == "reading":
+  print("SHEEESH i love it too")
+  if Things == "sleeping":
+    print("i love it too")
+    if Things == "eating":
+      print("i love it too")
+      if Things == "watching tv":
+        print("i love it too")
+        if Things == "playing games":
+          print("i love it too")
+          if Things == "watching movies":
+            print("i love it too")
+            if Things == "playing with my friends":
+              print("i love it too")
+              if Things == "playing with my family":
+                print("i love it too")
+                if Things == "playing with my pets":
+                  print("i love it too")
+                  if Things == "playing with my siblings":
+                    print("i love it too")
+                    if Things == "playing with my pets":
+                      print("i love it too")
+                      if Things == "playing with my pets":
+                        print("i love it too")
+                        if Things == "playing with my pets":
+                          print("i love it too")
+                          if Things == "playing with my pets":
+                            print("i love it too")
+                            if Things == "playing with my pets":
+                              print
+print("Great!")
+print("So, " + nameUser + " today is " + todaysDate + " and your favorite activities are + Things")
